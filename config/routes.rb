b272774e5a33d8e0d26c38e1c86b9b67b872c48f
JuniimSI <jrfernandesti@gmail.com>
2021-03-30 17:58:46 -0300

    admin sign in and sign out

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  devise_for :admins, skip: [:registrations], controllers: {sessions: 'admins/sessions'}
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :admin do
+    #admin/posts - #admin/users
+    root to: "home#index"
+  end
+end
