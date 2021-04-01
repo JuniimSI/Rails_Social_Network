a9a1e1dcc5e13f00b461253a560a4203c47072c8
JuniimSI <jrfernandesti@gmail.com>
2021-04-01 10:56:48 -0300

    users sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {registrations: 'users/registrations'}
   devise_for :admins, skip: [:registrations], controllers: {sessions: 'admins/sessions'}
   
   namespace :admin do
     #admin/posts - #admin/users
     root to: "home#index"
     resources :admins
+    resources :users, only: :index
   end
 
   root to: "user/timeline#index"
